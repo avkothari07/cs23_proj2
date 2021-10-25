1ef8ae914b98c3571d84f55f774427998e196893
evabaranes01 <eva.baranes@tufts.edu>
2021-10-25 15:02:00 -0400

    background and bumpers
    Fixed the background, shrunk the car so it fits in the path, and added bumpers to prevent the car from moving outside the screen

--- Proj2/Assets/ArrowKeys.cs
+++ Proj2/Assets/ArrowKeys.cs
@@ -1,12 +1,16 @@
  using UnityEngine;
  using System.Collections;
+ using System.Collections.Generic;
  
  public class ArrowKeys : MonoBehaviour {
      
      float speed = 1.2f;
      
      void Update() {
-         var movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-         transform.position += movement * speed * Time.deltaTime;
+        var movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += movement * speed * Time.deltaTime;
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -5.5f, 15f),
+     						Mathf.Clamp(transform.position.y, -8f, 4f),
+     						transform.position.z);
      }
  }
