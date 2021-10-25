9277c50c29ac18e91f9a46380f4d6b34b2bf201b
evabaranes01 <eva.baranes@tufts.edu>
2021-10-25 15:34:14 -0400

    bumpers added
    added a bunch of bumpers around the path

--- Proj2/Assets/ArrowKeys.cs
+++ Proj2/Assets/ArrowKeys.cs
@@ -9,8 +9,5 @@
      void Update() {
         var movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         transform.position += movement * speed * Time.deltaTime;
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -5.5f, 15f),
-     						Mathf.Clamp(transform.position.y, -8f, 4f),
-     						transform.position.z);
      }
  }
